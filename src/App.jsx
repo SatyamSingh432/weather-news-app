5dec7f2dfb204793498c570eb3049ae921b9d20d
Satyam Singh <satyamrajput7239@gmail.com>
2025-05-29 15:55:54 +0530

    err- news api need paid subs

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useState } from 'react'
 import Weather from "./pages/Weather";
-import News from "./pages/News";
+// import News from "./pages/News";
 import './App.css'
 function App() {
 
   return (
      <BrowserRouter>
       <Routes>
-        {/* <Route path="/" element={<Weather />} /> */}
-        <Route path="/" element={<News />} />
+        <Route path="/" element={<Weather />} />
+        {/* <Route path="/" element={<News />} /> */}
 
       </Routes>
     </BrowserRouter>
