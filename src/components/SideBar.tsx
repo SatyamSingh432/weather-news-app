6fe3c3d0c1f9c39d73545c5e09b985a64869c3be
Satyam Singh <satyamrajput7239@gmail.com>
2025-05-29 20:23:55 +0530

    fix:bug

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -3,7 +3,7 @@ const Sidebar = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="h-[120px] w-25 bg-gray-900 text-white p-2 flex flex-col gap-4">
+    <div className="h-[120px] w-25 bg-[rgb(13,14,55)] text-white p-2 flex flex-col gap-4">
       <div
         onClick={() => navigate("/news")}
         className="flex items-center gap-2 p-2 rounded hover:bg-gray-700 cursor-pointer"
--- src/services/news.ts
+++ src/services/news.ts
@@ -1,12 +1,12 @@
 import axios from "axios";
 
-const API_KEY = "7213719b20494d53aed5d4873250e6ac";
+// const API_KEY = "7213719b20494d53aed5d4873250e6ac";
 
 export const getNews = async () => {
   const res = await axios.get(
     
-    `https://newsapi.org/v2/top-headlines/sources?apiKey=${API_KEY}`
+    `https://yardstick-4l5u.onrender.com/api/transactionamt/news-sources`
   );
-  
-  return res.data.sources;
+  // console.log(res.data)
+  return res.data;
 };
